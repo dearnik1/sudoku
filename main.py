26ea7860a1e352c6e54bc92ee3c07f75214433fa
Nik Kho <nikita@creditninja.com>
2022-02-16 14:51:57 -0600

    upgrade v2

--- main.py
+++ main.py
@@ -72,28 +72,53 @@ class Sudoku:
         return 1
 
     def get_adjacent_elements_coordinates(self, row, column):
+
         # res = set()
         # for i in range(9):
-        #     res.add([row, i])
-        # print(res)
+        #     pair = (i, column)
+        #     res.add(pair)
+        #     pair = (row, i)
+        #     res.add(pair)
+        # x = int(row / 3)
+        # y = int(column / 3)
+        # for i in range(9):
+        #     pair = (x * 3 + int(i / 3), y * 3 + i % 3)
+        #     res.add(pair)
+        res = set()
+        res.update(self.get_adjacent_in_column(row, column))
+        res.update(self.get_adjacent_in_row(row, column))
+        res.update(self.get_adjacent_in_subgrid(row, column))
+        return res
+
+    def get_adjacent_in_row(self, row, column):
+        res = set()
+        for i in range(9):
+            pair = (row, i)
+            res.add(pair)
+        return res
+
+    def get_adjacent_in_column(self, row, column):
+
         res = set()
         for i in range(9):
             pair = (i, column)
             res.add(pair)
-            pair = (row, i)
-            res.add(pair)
+        return res
+
+    def get_adjacent_in_subgrid(self, row, column):
         x = int(row / 3)
         y = int(column / 3)
+        res = set()
         for i in range(9):
             pair = (x * 3 + int(i / 3), y * 3 + i % 3)
             res.add(pair)
-        # remove (row, column)?
         return res
 
     def upgrade(self):
         changes_made = True
-        while(changes_made):
+        while changes_made:
             changes_made = False
+            # fill elements that have the only 1 possible value remaining
             for row in range(9):
                 for column in range(9):
                     el = self.elements[row][column]
@@ -104,6 +129,44 @@ class Sudoku:
                                 self.fill(row, column, x)
                                 print('Adding {} to {},{}'.format(x, row, column))
                                 break
+            # fill elements that can have a number that adjacent elements cannot...
+            for row in range(9):
+                for column in range(9):
+                    el = self.elements[row][column]
+                    if el.is_empty:
+                        for x in range(1, 10):
+                            if x not in el.impossible_values:
+                                # for each possible value check if it is impossible in adjacent elements:
+                                counter = 0
+                                # traverse in row
+                                for r,c in self.get_adjacent_in_row(row, column):
+                                    if x in self.elements[r][c].impossible_values:
+                                        counter += 1
+                                if counter == 8:
+                                    self.fill(row, column, x)
+                                    print('Adding {} to {},{}'.format(x, row, column))
+                                    changes_made = True
+                                    break
+                                counter = 0
+                                # traverse in column
+                                for r, c in self.get_adjacent_in_column(row, column):
+                                    if x in self.elements[r][c].impossible_values:
+                                        counter += 1
+                                if counter == 8:
+                                    self.fill(row, column, x)
+                                    print('Adding {} to {},{}'.format(x, row, column))
+                                    changes_made = True
+                                    break
+                                counter = 0
+                                # traverse in subgrid
+                                for r, c in self.get_adjacent_in_subgrid(row, column):
+                                    if x in self.elements[r][c].impossible_values:
+                                        counter += 1
+                                if counter == 8:
+                                    self.fill(row, column, x)
+                                    print('Adding {} to {},{}'.format(x, row, column))
+                                    changes_made = True
+                                    break
 
 
     def display(self):
